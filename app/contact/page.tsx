7cd577574779141a1c987606b78c470697c8d56d
Volltix <volltixR200@gmail.com>
2024-06-04 10:03:00 +0200

    fixed form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             </div>
 
             <div
-                className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                className=" left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                 <form onSubmit={handleSubmit}
                       className="bg-transparent dark:inherit shadow-md rounded px-8 pt-6 pb-8 mb-4">
                     <div className="mb-4">
